d36b2bb6bfcae72a54ba2bcde31b6a1af66ddcb1
ju_zkai <2360263057@qq.com>
2017-11-19 17:21:27 +0800

    添加home页轮播图

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,52 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ActionSheetController, Slides } from 'ionic-angular';
+import { ViewChild } from '@angular/core';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  @ViewChild(Slides) slides: Slides;
 
-  constructor(public navCtrl: NavController) {
+  ngAfterViewInit() {
+    this.slides.autoplay = 3000;
+    this.slides.loop = true;
+    this.slides.autoplayDisableOnInteraction = false;
+  }
+
+  constructor(public navCtrl: NavController, public actionSheetCtrl: ActionSheetController) {
 
   }
 
+  open = () => {
+    let actionSheet = this.actionSheetCtrl.create({
+      title: 'Modify your album',
+      buttons: [
+        {
+          text: 'Destructive',
+          role: 'destructive',
+          handler: () => {
+            console.log('Destructive clicked');
+          }
+        },
+        {
+          text: 'Archive',
+          handler: () => {
+            console.log('Archive clicked');
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+
+    actionSheet.present();
+  }
 }
+
