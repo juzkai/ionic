e998c76f2c9de33fc601bea26b54fdb8fe8886ca
ju_zkai <2360263057@qq.com>
2018-01-15 14:57:24 +0800

    修改图片路径，均已index.html为基准的相对路径

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -11,15 +11,15 @@ export class ContactPage {
    *  功能菜单
    */
    item =[
-    {img:"../../assets/imgs/item-unit.png",title:'item1'},
-    {img:"../../assets/imgs/item-unit.png",title:'item1'},
-    {img:"../../assets/imgs/item-unit.png",title:'item1'},
-    {img:"../../assets/imgs/item-unit.png",title:'item1'},
-    {img:"../../assets/imgs/item-unit.png",title:'item1'},
-    {img:"../../assets/imgs/item-unit.png",title:'item1'},
-    {img:"../../assets/imgs/item-unit.png",title:'item1'},
-    {img:"../../assets/imgs/item-unit.png",title:'item1'},
-    {img:"../../assets/imgs/item-unit.png",title:'item1'}
+    {img:"./assets/imgs/item-unit.png",title:'item1'},
+    {img:"./assets/imgs/item-unit.png",title:'item2'},
+    {img:"./assets/imgs/item-unit.png",title:'item3'},
+    {img:"./assets/imgs/item-unit.png",title:'item4'},
+    {img:"./assets/imgs/item-unit.png",title:'item5'},
+    {img:"./assets/imgs/item-unit.png",title:'item6'},
+    {img:"./assets/imgs/item-unit.png",title:'item7'},
+    {img:"./assets/imgs/item-unit.png",title:'item8'},
+    {img:"./assets/imgs/item-unit.png",title:'item9'}
   ]
   
   constructor(public navCtrl: NavController) {
